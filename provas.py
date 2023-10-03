eccb6a20aa542367b3f21ede7805377914443ae3
franciele099 <146848954+franciele099@users.noreply.github.com>
2023-10-03 11:18:01 -0300

    prova
    franciele e joao pedro

---
+++ provas.py
@@ -0,0 +1,7 @@
+n1=int(input("digite sua nota"))
+n2=int(input("digite sua nota"))
+n3=int(input("digite sua nota"))
+n4=int(input("digite sua nota"))
+n5=int(input("digite sua nota"))
+r=(n1+n2+n3+n4+n5)/5
+print("sua media e:",r)
